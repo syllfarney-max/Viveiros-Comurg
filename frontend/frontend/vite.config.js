13ba683dc7dcafc90381ee66689c97525cd135b8
syllfarney-max <syllfarney@gmail.com>
2025-10-03 18:40:52 -0300

    Add files via upload

---
+++ frontend/frontend/vite.config.js
@@ -0,0 +1,25 @@
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: '0.0.0.0',
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'viveiro-comurg-frontend.onrender.com'
+    ]
+  },
+  preview: {
+    port: 4173,
+    host: '0.0.0.0',
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'viveiro-comurg-frontend.onrender.com'
+    ]
+  }
+})
