37f9ebfac3db741c23ba89b4f7db95db368e8a6c
syllfarney-max <syllfarney@gmail.com>
2025-10-03 13:37:57 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// Hosts liberados
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
     host: "0.0.0.0",
+    port: 3000,
+  },
+  preview: {
+    port: 10000,
     allowedHosts: [
       "localhost",
-      "127.0.0.1",
       "viveiro-comurg-frontend.onrender.com",
+      "viveiro-comurg-frontend-34cj.onrender.com",
       "viveiro-comurg-frontend-jrpv.onrender.com"
     ]
   }
-});
+})
